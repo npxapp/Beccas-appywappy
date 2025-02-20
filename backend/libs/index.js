fbb88f0d598696b3c19dc0ce503f3f629013dcf6
Your Name <youremail@example.com>
2025-02-20 20:37:58 +0000

    Delete all files

--- backend/libs/index.js
+++
@@ -1,8 +0,0 @@
-import "./common/env.js";
-import routes from "./routes.js";
-import ExpressServer from "./server.js";
-
-export default new ExpressServer()
-  .router(routes)
-  .listen(process.env.PORT)
-  .initCron();
--- src/libs/atoms.js
+++
@@ -1,7 +0,0 @@
-// libs/atoms.jsx
-import { atom } from "recoil";
-
-export const selectedPlanState = atom({
-  key: "selectedPlanState", // unique ID (with respect to other atoms/selectors)
-  default: undefined, // default value (aka initial value)
-});
