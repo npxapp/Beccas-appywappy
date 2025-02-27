fc14a73e0035e95aabff9ec036090b1214179433
Your Name <youremail@example.com>
2025-02-27 14:24:02 +0000

    Refresh all files

--- backend/libs/api/webhooks/webhook.router.js
+++
@@ -1,7 +0,0 @@
-import * as express from "express";
-import { wrap } from "../../common/exceptions.js";
-import webhookController from "./webhook.controller.js";
-
-export default express
-  .Router()
-  .post("/", wrap(webhookController.handleWebhook));
